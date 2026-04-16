46a6afd38b740e614b618572149b9a22b22b1271
Lyke De Leon <deleonlyke006@gmail.com>
2026-04-16 23:52:07 +0800

    Update max_flow.cpp

--- model/max_flow.cpp
+++ model/max_flow.cpp
@@ -5,10 +5,15 @@ int main() {
     int minCapacity = 16;
     int maxCapacity = 19;
 
+    cout << "=== MAX FLOW MODEL ===\n";
     cout << "Evacuation Capacity per Trip: "
-         << minCapacity << " to " << maxCapacity << " persons" << endl;
+         << minCapacity << " to " << maxCapacity << " persons\n";
 
-    cout << "This represents the MAX FLOW of the system." << endl;
+    cout << "Interpretation:\n";
+    cout << "This represents the maximum number of evacuees\n";
+    cout << "that can be transported per cycle.\n";
+
+    cout << "\nBottleneck: Transportation capacity\n";
 
     return 0;
 }
